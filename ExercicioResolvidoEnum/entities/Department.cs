61b2a940e6e10dd119eb4bca2100cadc46dc6dad
Pedro Henrique da Paixão  Carneiro <pedrohenriquedapaixao@outlook.com>
2021-06-28 20:16:18 -0300

    Exercicio Resolvido Enum/Composição

---
+++ ExercicioResolvidoEnum/entities/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioResolvidoEnum.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+        public Department(string name){
+            this.Name = name;
+        }
+
+
+    }
+}
