0a4c3e49be00f9ad8dac5e786ec940e8c91a36f6
Pedro Henrique da Paixão  Carneiro <pedrohenriquedapaixao@outlook.com>
2021-06-28 22:32:27 -0300

    Exercício Proposto ( Enumerações e Composição)

---
+++ ExercicioPropostoEnumeracaoComposicao/Entities/Order.cs
@@ -0,0 +1,64 @@
+﻿using ExercicioPropostoEnumeracaoComposicao.Entities.Utils;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoEnumeracaoComposicao.Entities
+{
+    class Order
+    {
+        public DateTime Moment = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set;} = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order()
+        {
+        }
+
+        public Order(OrderStatus status, List<OrderItem> items, Client client)
+        {
+            Status = status;
+            Items = items;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0;
+            foreach (OrderItem Item in Items)
+            {
+                total += Item.SubTotal();
+            }
+
+            return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Order moment: {Moment}");
+            sb.AppendLine($"Order status: {Status}");
+            sb.AppendLine($"Client: {Client.Name} ({Client.BirthDate.ToString("dd/MM/yyyy")}) - {Client.Email}");
+            sb.AppendLine("Order items");
+            foreach(OrderItem item in Items)
+            {
+                sb.AppendLine($"{item.Product.Name}, ${item.Price.ToString("F2", CultureInfo.InvariantCulture)}, Quantity: {item.Quantity}, Subtotal: ${item.SubTotal().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            sb.AppendLine($"Total price: ${Total().ToString("F2",CultureInfo.InvariantCulture)}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ExercicioPropostoEnumeracaoComposicao/Program.cs
@@ -0,0 +1,56 @@
+﻿using ExercicioPropostoEnumeracaoComposicao.Entities.Utils;
+using System.Globalization;
+using System;
+using ExercicioPropostoEnumeracaoComposicao.Entities;
+
+namespace ExercicioPropostoEnumeracaoComposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("How many items to this order? ");
+            int howManyItems = int.Parse(Console.ReadLine());
+
+            Order order = new Order();
+            
+            order.Status = status;
+
+            order.Client = client;
+            
+            for(int i = 0; i < howManyItems; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product produto = new Product(productName, productPrice);
+                OrderItem orderItem = new OrderItem(quantity,produto.Price,produto);
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(order);
+        }
+    }
+}
