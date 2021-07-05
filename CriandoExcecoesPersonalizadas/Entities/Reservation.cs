311c990a384a07331cf2b818e44166079d93d0ab
Pedro Henrique <pedrohenriquedapaixao@outlook.com>
2021-07-05 16:09:21 -0300

    143. Criando exceções personalizadas - PARTE 1

---
+++ CriandoExcecoesPersonalizadas/Entities/Reservation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoExcecoesPersonalizadas.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+    
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn,DateTime checkOut)
+        {
+            if(checkIn > CheckIn && checkOut > checkIn)
+            {
+                CheckIn = checkIn;
+                CheckOut = checkOut;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Reservation: Room {RoomNumber}, check-in: {CheckIn.ToString("dd/MM/yyy")}, check-out: {CheckOut.ToString("dd/MM/yyy")}, {Duration()} nights";
+        }
+    }
+}
---
+++ CriandoExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,44 @@
+﻿using CriandoExcecoesPersonalizadas.Entities;
+using System;
+
+namespace CriandoExcecoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+
+                Console.WriteLine(reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn,checkOut);
+
+                Console.WriteLine(reservation);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"ERROR: {e.Message}");
+            }
+
+            
+
+
+        }
+    }
+}
